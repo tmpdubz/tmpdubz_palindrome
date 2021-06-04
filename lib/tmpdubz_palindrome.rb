6e292323aa046c2b9d762adb347deaa6faa80153
tmpdubz <t.m.dubiel@gmail.com>
2021-06-03 20:17:22 -0400

    Finished working through the chapter, tests passing

--- lib/tmpdubz_palindrome.rb
+++ lib/tmpdubz_palindrome.rb
@@ -7,21 +7,10 @@ class String
     processed_content == processed_content.reverse
   end
 
-  # Returns the letters in the string.
-  def letters
-    the_letters = []
-    for i in 0..self.length - 1
-      if (self[i].match(/[a-zA-Z]/))
-        the_letters << self[i]
-      end
-    end
-    the_letters.join
-  end
-
   private
 
     # Returns content for palindrome testing.
     def processed_content
-      self.letters.downcase
+      scan(/[a-z]/i).join.downcase
     end
 end
